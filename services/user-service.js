34923ba604ac106e15aa617188ec7540cb3ee551
SergioTobon <sestebantmontoya@gmail.com>
2024-09-28 20:12:54 -0500

    se crea el service de crear usuario

--- services/user-service.js
+++ services/user-service.js
@@ -1,9 +1,34 @@
+import { User } from '../models/user.js';
 import UserRepository from '../repositories/user-repository.js';
 
 class UserService{
     constructor(userRepository){
         this.userRepository = new UserRepository();
     }
+
+    async createUser(user){
+        if(!(user instanceof User)){
+            throw new Error('Invalid user object');
+        }
+
+        if(user.username == null){
+            throw new Error('Username is required');
+        }
+        if(user.password == null){
+            throw new Error('Password is required');
+        }
+        if(user.email == null){
+            throw new Error('Email is required');
+        }
+        
+
+
+        
+
+
+
+    }
+
 }
 
 export default UserService;
